129563652552f7598c6d0cffac9c2236ef84b667
dmatthams <dmatthams@users.noreply.github.com>
2020-11-16 14:09:54 +0000

    Simplify download for now

--- src/variables/DamVariable.php
+++ src/variables/DamVariable.php
@@ -34,29 +34,10 @@ class DamVariable
         return Craft::$app->getProjectConfig()->get('users')['allowPublicRegistration'];
     }
 
-    public function download($assets)
+    public function download($assetsIds)
     {
-        $ids = [];
-
-        // Detect if it's more than one asset
-        if (is_array($assets)) {
-            // If it's a DAM asset
-            if($assets[0] instanceof DamAsset) {
-                foreach ($assets as $asset) {
-                    $ids[] = $asset->id;
-                }
-            } else {
-                throw new Exception(Craft::t('dam', 'Download twig variable expects instances of DamAsset'));
-            }
-        } else {
-            if($assets instanceof DamAsset) {
-                $ids[] =  $assets->id;
-            } else {
-                throw new Exception(Craft::t('dam', 'Download twig variable expects instances of DamAsset'));
-            }
-        }
         return UrlHelper::actionUrl('/dam/download/', [
-            'files' => $ids
+            'files' => $assetsIds
         ]);
     }
 }
